da3e62b18622abb4c953a3c3e4451851ddce5702
Daniel Zou <zou23cn@gmail.com>
2019-07-02 19:21:17 +0800

    create

---
+++ ex5.py
@@ -0,0 +1,11 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 75 # inches
+my_weight = 180 #lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair ='Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"")
