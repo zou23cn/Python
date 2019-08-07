4a656dbfb399065b5e7cf050ab7dc21d3e605254
Daniel Zou <zou23cn@gmail.com>
2019-08-08 00:58:10 +0800

    ex19

---
+++ ex19.py
@@ -0,0 +1,10 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print(f"You have {cheese_count} cheeses!")
+    print(f"You have {boxes_of_crackers} boxes of crackers!")
+    print("Man that's enough for a party!")
+    print("Get a blanket. \n")
+
+
+print("We can just give the function numbers directly:")
+cheese_and_crackers(20, 30)
+
