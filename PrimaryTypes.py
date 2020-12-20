faff0c3d442c1d61eb324e5694bfd5572caee3e2
Daniel Zou <zou23cn@gmail.com>
2020-12-21 00:12:30 +0800

    12月21日python学校

---
+++ PrimaryTypes.py
@@ -0,0 +1,15 @@
+# Python数据类型
+# Number  数字
+# int, float, bool, complex（复数）
+intVar = 1
+print(intVar)
+
+floatVar = 1.1234
+print(floatVar)
+
+complexVar = 3+25j
+print(complexVar)
+
+# String  字符串
+str = "字符串"
+print(str)
---
+++ ex24.py
@@ -0,0 +1,20 @@
+print("Let's practice eveything")
+print('You\'d need to know \'bout escapes with \\ that do:')
+print('\n newlines and \t tabs.')
+
+poem = """
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires and explanttion
+\n\twhere there is none
+"""
+
+print("-------------")
+print(poem)
+print("-------------")
+
+
+five = 10 - 2 + 3 - 6
+print(f"This should be five: {five}")
